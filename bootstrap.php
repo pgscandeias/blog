fa88383d1677f9844e476006255d0d36a3692ff6
Pedro Candeias <pgscandeias@gmail.com>
2013-02-20 21:07:29 +0000

    Use credentials when connecting to mongodb

--- bootstrap.php
+++ bootstrap.php
@@ -11,11 +11,14 @@ require_once APP_ROOT . '/view.php';
 # Business
 require_once APP_ROOT . '/models.php';
 
-# Db
-$mongo = new MongoClient();
-Model::$db = $mongo->selectDB('blog');
-
-
 # Init
 $app = new App();
 $view = new View(APP_ROOT . '/views/');
+
+# Db connection
+$dsn = 'mongodb://['.$app->config->get('db_username').':'.$app->config->get('db_password').']';
+$dsn.= '@'.$app->config->get('db_host').':'.$app->config->get('db_port');
+$dsn.= '/'.$app->config->get('db_name');
+$mongo = new MongoClient();
+Model::$db = $mongo->selectDB('blog');
+
