6ac09842a2899dcdf1c0385ca9673863d007f670
Pedro Candeias <pgscandeias@gmail.com>
2013-03-10 20:43:33 +0000

    Little bit of Responsive

--- views/_header.tpl.php
+++ views/_header.tpl.php
@@ -7,7 +7,8 @@
             else echo "Pedro Gil Candeias";
         ?></title>
 
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link href='http://fonts.googleapis.com/css?family=Roboto:300,500' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400' rel='stylesheet' type='text/css'>
@@ -16,21 +17,19 @@
     </head>
 
     <body>
-        <nav>
-            <header class='site-title h'>
-                <a href='/'>
-                    <img class='avatar' src='/assets/avatar.jpg'>
-                    <span class='name'>
-                        Pedro Gil Candeias
-                    </span>
-                </a>
-            </header>
+        <header class='site-header'>
+            <a href='/' class='site-title h'>
+                <img class='avatar' src='/assets/avatar.jpg'>
+                <span class='name'>
+                    Pedro Gil Candeias
+                </span>
+            </a>
 
             <p class='intro'>
                 web app developer
             </p>
 
-            <ul class='unstyled side-nav'>
+            <ul class='unstyled header-nav'>
                 <li><a href='/'>ramblings</a></li>
                 <? if (isset($pages)): foreach ($pages as $p): ?>
                     <li><a href='<?= $p->url() ?>'>
@@ -38,6 +37,6 @@
                     </a></li>
                 <? endforeach; endif; ?>
             </ul>
-        </nav>
+        </header>
 
         <div class='container'>
